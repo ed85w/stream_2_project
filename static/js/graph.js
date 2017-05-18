1ea88a97ba9bba12f31d8991569ba60411e03dbb
ed85w <ed.walker@hotmail.co.uk>
2017-05-18 21:01:15 +0100

    date issue sorted - goals barchart added

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,24 +1,26 @@
 /**
  * Created by Ed on 13/05/2017.
  */
+
 queue()
-   .defer(d3.json, "/projectTest1/testData")
+   .defer(d3.json, "/projectTest/testData")
    .await(makeGraphs);
  
-function makeGraphs(error, projectsJson) {
+function makeGraphs(error, jsonData) {
  
-   //Clean projectsJson data
-   var projectTest1 = projectsJson;
-   var dateFormat = d3.time.format("%d-%m-%Y");
+   //Clean jsonData data
+   var projectTest1 = jsonData;
+   var dateFormat = d3.time.format("%d/%m/%Y");
    projectTest1.forEach(function (d) {
        d["fixture_date"] = dateFormat.parse(d["fixture_date"]);
-       d["fixture_date"].setDate(1);
-       d["home_goals"] = +d["home_goals"];
+       // d["fixture_date"].setDate(1);
+       // d["home_goals"] = +d["home_goals"];
+       d.totalGoals = d["home_goals"] + d["away_goals"]
    });
 
       //Create a Crossfilter instance
    var ndx = crossfilter(projectTest1);
- 
+
    //Define Dimensions
    var dateDim = ndx.dimension(function (d) {
        return d["fixture_date"];
@@ -27,8 +29,55 @@ function makeGraphs(error, projectsJson) {
        return d["home_team"];
    });
 
-   // Calculate metrics
-   var homeGoalsByDate = dateDim.group();
+   var awayTeamDim = ndx.dimension(function (d) {
+       return d["away_team"];
+   });
+
+
+
+   //calculate metrics
+    var totalHomeGoalsByDate = dateDim.group().reduceSum(function(d){
+        return d["home_goals"];
+    })
+    var totalAwayGoalsByDate = dateDim.group().reduceSum(function(d){
+        return d["away_goals"];
+    })
+
+    var totalGoalsDim = dateDim.group().reduceSum(function (d) {
+        return d.total;
+    });
+    var homeTeamGroup = homeTeamDim.group();
+
+    var all = ndx.groupAll();
+
+    var max_goals = totalHomeGoalsByDate.top(1)[0].value;
+
+       //Define values (to be used in charts)
+   var minDate = dateDim.bottom(1)[0]["fixture_date"];
+   var maxDate = dateDim.top(1)[0]["fixture_date"];
+
+    // define charts
+    var timeChart = dc.barChart("#time-chart");
+
+    selectField = dc.selectMenu('#menu-select')
+       .dimension(homeTeamDim)
+       .group(homeTeamGroup);
+
+     timeChart
+       .width(800)
+       .height(200)
+       .margins({top: 10, right: 50, bottom: 30, left: 50})
+       .dimension(dateDim)
+       .group(totalHomeGoalsByDate, "home")
+         .stack(totalAwayGoalsByDate, "away")
+
+       .transitionDuration(500)
+       .x(d3.time.scale().domain([minDate, maxDate]))
+       .elasticY(true)
+       .xAxisLabel("Year")
+       .yAxis().ticks(4);
+
+    dc.renderAll();
 
 };
 
