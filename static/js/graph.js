e287a21d28ed553ebe5a130539a9f9f4f259df0d
ed85w <ed.walker@hotmail.co.uk>
2017-05-20 13:48:48 +0100

    gameweek rather than fixture date used

--- static/js/graph.js
+++ static/js/graph.js
@@ -10,51 +10,45 @@ function makeGraphs(error, jsonData) {
  
    //Clean jsonData data
    var projectTest1 = jsonData;
-   var dateFormat = d3.time.format("%d/%m/%Y");
    projectTest1.forEach(function (d) {
-       d["fixture_date"] = dateFormat.parse(d["fixture_date"]);
-       // d["fixture_date"].setDate(1);
-       // d["home_goals"] = +d["home_goals"];
-       d.totalGoals = d["home_goals"] + d["away_goals"]
+       d["total_goals"] = d["home_goals"] + d["away_goals"]
    });
 
       //Create a Crossfilter instance
    var ndx = crossfilter(projectTest1);
 
    //Define Dimensions
-   var dateDim = ndx.dimension(function (d) {
-       return d["fixture_date"];
-   });
+    var matchweekDim = ndx.dimension(function (d) {
+        return d["Matchweek"]
+    });
    var homeTeamDim = ndx.dimension(function (d) {
        return d["home_team"];
    });
-
    var awayTeamDim = ndx.dimension(function (d) {
        return d["away_team"];
    });
 
 
-
    //calculate metrics
-    var totalHomeGoalsByDate = dateDim.group().reduceSum(function(d){
+    var totalHomeGoalsByDate = matchweekDim.group().reduceSum(function(d){
         return d["home_goals"];
-    })
-    var totalAwayGoalsByDate = dateDim.group().reduceSum(function(d){
+    });
+    var totalAwayGoalsByDate = matchweekDim.group().reduceSum(function(d){
         return d["away_goals"];
+    });
+    var totalGoalsByDate = matchweekDim.group().reduceSum(function(d){
+        return d["total_goals"]
     })
 
-    var totalGoalsDim = dateDim.group().reduceSum(function (d) {
-        return d.total;
-    });
+
+    // groups
     var homeTeamGroup = homeTeamDim.group();
+
+    var awayTeamGroup = awayTeamDim.group();
 
     var all = ndx.groupAll();
 
     var max_goals = totalHomeGoalsByDate.top(1)[0].value;
-
-       //Define values (to be used in charts)
-   var minDate = dateDim.bottom(1)[0]["fixture_date"];
-   var maxDate = dateDim.top(1)[0]["fixture_date"];
 
     // define charts
     var timeChart = dc.barChart("#time-chart");
@@ -63,19 +57,18 @@ function makeGraphs(error, jsonData) {
        .dimension(homeTeamDim)
        .group(homeTeamGroup);
 
-     timeChart
-       .width(800)
-       .height(200)
-       .margins({top: 10, right: 50, bottom: 30, left: 50})
-       .dimension(dateDim)
-       .group(totalHomeGoalsByDate, "home")
-         .stack(totalAwayGoalsByDate, "away")
-
-       .transitionDuration(500)
-       .x(d3.time.scale().domain([minDate, maxDate]))
-       .elasticY(true)
-       .xAxisLabel("Year")
-       .yAxis().ticks(4);
+    timeChart
+        .width(1500)
+        .height(200)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(matchweekDim)
+        .group(totalHomeGoalsByDate, "home")
+        // .stack(totalAwayGoalsByDate, "away")
+        .transitionDuration(500)
+        .x(d3.time.scale().domain([1, 38]))
+        .elasticY(true)
+        .xAxisLabel("Matchweek")
+        .yAxis().ticks(10);
 
     dc.renderAll();
 
