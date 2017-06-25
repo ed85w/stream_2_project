676667bbad7f1269024fc23d9bb1d32305a8b5a3
ed85w <ed.walker@hotmail.co.uk>
2017-06-25 22:01:31 +0100

    working on goals scored details table

--- static/js/graph.js
+++ static/js/graph.js
@@ -3,20 +3,13 @@
  */
 
 // const TEAM_COLORS = {
-//     'Manchester': ['#34231343', '#34213414'],
-//
+//         'Arsenal': ['#e60005','#fffeff'],
+//         'Manchester': ['#34231343', '#34213414'],
 // };
 
-// TEAM_COLORS[d['team_name']]
+ // TEAM_COLORS[d['team_name']]
 
-// variables for tweaking the responsiveness of the dc.js charts
-var maxChartWidth = 750;
-var mediumChartWidth = 580;
-var smallChartWidth = 440;
-var extraSmallChartWidth = 330;
-var largeViewportSwitch = 768;
-var mediumViewportSwitch = 596;
-var smallViewportSwitch = 480;
+
 
 
 queue()
@@ -24,20 +17,13 @@ queue()
    .await(makeGraphs);
  
 function makeGraphs(error, jsonData) {
- 
-   //Clean jsonData data
-   // var projectTest1 = jsonData;
 
-   // projectTest1.forEach(function (d) {
-   //     // d["attendance"] = +d["attendance"];
-   // });
-
-      //Create a Crossfilter instance
+   //Create a Crossfilter instance
    var ndx = crossfilter(jsonData);
 
    //Define Dimensions
     var matchweekDim = ndx.dimension(function (d) {
-        return d["matchweek"]
+        return d["matchweek"];
     });
     var teamDim = ndx.dimension(function (d){
         return d['team'];
@@ -49,16 +35,12 @@ function makeGraphs(error, jsonData) {
             return "Away";
         }
     });
-    var attendanceDim = ndx.dimension(function (d){
-        return d['attendance'];
-    });
     var totalShotsForDim = ndx.dimension(function(d){
         return d["total_shots_for"];
     });
     var totalShotsAgainstDim = ndx.dimension(function(d){
         return d["total_shots_against"];
     });
-
 
    //calculate metrics
     var totalGoalsForByDate = matchweekDim.group().reduceSum(function(d){
@@ -138,8 +120,7 @@ function makeGraphs(error, jsonData) {
 
     var homeAwayGroup = homeAwayDim.group();
 
-    // var all = ndx.groupAll();
-
+    var matchweekGroup = matchweekDim.group();
 
     //min and max values to be used in charts
     var minWeek = matchweekDim.bottom(1)[0]["matchweek"];
@@ -151,10 +132,15 @@ function makeGraphs(error, jsonData) {
     var minShotsAgainst = totalShotsAgainstDim.bottom(1)[0]["total_shots_against"];
     var maxShotsAgainst = totalShotsAgainstDim.top(1)[0]["total_shots_against"];
 
+    // set initial widths based on screen size
+    var goalsChartWidth = $(".goals-chart-container").width();
+    var pieChartWidth = $(".pie-chart-container").width();
+    var shotsChartsWidth = $(".shots-chart-container").width();
+
     // define charts
     var goalsChart = dc.barChart("#goals-chart");
     var totalShotsForChart = dc.barChart("#shots-for-chart");
-    var totalShotsAgainstChart = dc.barChart("#shots-against-chart")
+    var totalShotsAgainstChart = dc.barChart("#shots-against-chart");
     var pieChart = dc.pieChart("#home-away-goals-chart");
     var avgAttendanceND = dc.numberDisplay("#avg-attendance");
     var shotsToGoalsScoredND = dc.numberDisplay("#shots-to-goals-scored");
@@ -162,34 +148,28 @@ function makeGraphs(error, jsonData) {
     var totalGoalsForND = dc.numberDisplay("#total-goals-for");
     var totalGoalsAgainstND = dc.numberDisplay("#total-goals-against");
 
-    // set initial widths based on screen size
-    var chartWidth;
-    if (matchMedia) {
-        var mq1 = window.matchMedia("(min-width: " + largeViewportSwitch + "px)");
-        if (mq1.matches) {
-            chartWidth = maxChartWidth;
-        } else {
-            var mq2 = window.matchMedia("(min-width: " + mediumViewportSwitch + "px)");
-            if (mq2.matches) {
-                chartWidth = mediumChartWidth;
-            } else {
-                var mq3 = window.matchMedia("(min-width: " + smallViewportSwitch + "px)");
-                if (mq3.matches) {
-                    chartWidth = smallChartWidth;
-                } else {
-                    chartWidth = extraSmallChartWidth;
-                }
-            }
-        }
-    }
-
+    var goalsScoredTab = dc.dataTable("#goals-scored-table");
 
     selectField = dc.selectMenu('#menu-select')
        .dimension(teamDim)
        .group(teamGroup);
 
+    //remove key from selectField dropdown
+    selectField.title(function(d){
+        return d.key;
+    });
+
+    $("#menu-select").change(function() {
+        // var teamColour1 = selectField.value[0];
+        // $("body").css("background-color","blue")
+        a = d3.select("#menue-select").text(selectField.filters());
+
+        // b = $("#menu-select").val();
+        alert(a +' is the team selected');
+    });
+
     goalsChart
-        .width(chartWidth)
+        .width(goalsChartWidth)
         .height(250)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(matchweekDim)
@@ -203,47 +183,45 @@ function makeGraphs(error, jsonData) {
         .xAxisLabel("Matchweek")
         .yAxis().ticks(2);
 
+    pieChart
+        .height(250)
+        .width(pieChartWidth)
+        .radius(100)
+        .innerRadius(40)
+        .transitionDuration(1000)
+        .dimension(homeAwayDim)
+        .group(homeAwayGroup)
+        .legend(dc.legend().x(10).y(10).itemHeight(10).gap(5));
+
     totalShotsForChart
-        .width(450)
+        .width(shotsChartsWidth)
         .height(200)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({top: 10, right: 30, bottom: 30, left: 30})
         .dimension(totalShotsForDim)
-        .group(totalShotsForGroup, "Shots")
-        .ordinalColors(["#0dff0d","#b2131a"])
+        .group(totalShotsForGroup)
+        .ordinalColors(["#0dff0d"])
         .transitionDuration(500)
         // .color("#0dff0d")
         .x(d3.time.scale().domain([minShotsFor, maxShotsFor]))
-        .legend(dc.legend().x(450).y(10).itemHeight(13).gap(5))
         .elasticY(true)
         .xAxisLabel("Number of Shots")
         .yAxisLabel("Number of Games")
         .yAxis().ticks(4);
 
     totalShotsAgainstChart
-        .width(450)
+        .width(shotsChartsWidth)
         .height(200)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({top: 10, right: 30, bottom: 30, left: 30})
         .dimension(totalShotsAgainstDim)
-        .group(totalShotsAgainstGroup, "Shots")
-        .ordinalColors(["#0dff0d","#b2131a"])
+        .group(totalShotsAgainstGroup)
+        .ordinalColors(["#b2131a"])
         .transitionDuration(500)
         // .color("#0dff0d")
         .x(d3.time.scale().domain([minShotsAgainst, maxShotsAgainst]))
-        .legend(dc.legend().x(450).y(10).itemHeight(13).gap(5))
         .elasticY(true)
         .xAxisLabel("Number of Shots")
         .yAxisLabel("Number of Games")
         .yAxis().ticks(4);
-
-    pieChart
-        .height(250)
-        .width(360)
-        .radius(100)
-        .innerRadius(40)
-        .transitionDuration(1000)
-        .dimension(homeAwayDim)
-        .group(homeAwayGroup)
-        .legend(dc.legend().x(20).y(10).itemHeight(13).gap(5));
 
     avgAttendanceND
         .formatNumber(d3.format("d"))
@@ -297,41 +275,46 @@ function makeGraphs(error, jsonData) {
         .group(shotsToGoalsConceded)
         .transitionDuration(0);
 
+    goalsScoredTab
+        .dimension(matchweekDim)
+        // .group(matchweekGroup)
+            .group(function(d) { return d['_id_']; })
+        .columns([
+            function (d) { return d['matchweek']; },
+            function (d) { return d['team']; },
+            function (d) { return d['goal_details_for']; }
+        ])
+        // .columns([
+        //     'matchweek',
+        //     'goals_for_details'
+        // ])
+        .width(500);
+        // .height(500);
 
-
-    // // listen for browser resize
-    // $(window).on("resize", resizeWideCharts);
-    //
-    // // listen for orientation change
-    // var mq = window.matchMedia("(orientation: portrait)");
-    // mq.addListener(function(m) {
-    //     resizeWideCharts();
-    // });
-    //
-    // // test if the browser resize needs a chart resize
-    // function resizeWideCharts(){
-    //     var winWidth = $(window).width();
-    //     if (winWidth < smallViewportSwitch && chartWidth != extraSmallChartWidth){
-    //         chartResize(extraSmallChartWidth);
-    //     } else if (winWidth >= smallViewportSwitch && winWidth < mediumViewportSwitch && chartWidth != smallChartWidth){
-    //         chartResize(smallChartWidth);
-    //     } else if (winWidth >= mediumViewportSwitch && winWidth < largeViewportSwitch && chartWidth != mediumChartWidth){
-    //         if (chartWidth == maxChartWidth) {
-    //             dc.filterAll(); // this is the switch from interactive chart to menu selection
-    //         }
-    //         chartResize(mediumChartWidth);
-    //     } else if (winWidth >= largeViewportSwitch && chartWidth != maxChartWidth){
-    //         chartResize(maxChartWidth);
-    //     }
-    // }
-    //
-    // // do the chart resize
-    // function chartResize(width) {
-    //     console.log(width);
-    //     goalsChart.rescale();
-    // }
 
     dc.renderAll();
 
+    // listen for browser resize
+    $(window).on("resize", chartResize);
+
+    // function to resize chart based on bootstrap container
+    function chartResize(){
+        var goalsChartWidth = $(".goals-chart-container").width();
+        var pieChartWidth = $(".pie-chart-container").width();
+        var shotsChartsWidth = $(".shots-chart-container").width();
+        goalsChart
+            .width(goalsChartWidth);
+        pieChart
+            .width(pieChartWidth);
+        totalShotsForChart
+            .width(shotsChartsWidth);
+        totalShotsAgainstChart
+            .width(shotsChartsWidth);
+        dc.renderAll();
+    }
+
+
 };
+
+
 
